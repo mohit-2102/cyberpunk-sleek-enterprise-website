aab1292873fa15bdb64a4e6543a0705206aeb232
mohit-2102 <mohit.mathangi@gmail.com>
2025-09-11 17:34:37 +0530

    changed something

--- backend/utils/hash.js
+++
@@ -1,9 +0,0 @@
-// utils/hash.js
-import bcrypt from 'bcryptjs';
-
-const password = 'Admin@123';
-const saltRounds = 10;
-
-bcrypt.hash(password, saltRounds).then(hash => {
-  console.log('Hashed password:', hash);
-});
